ef51edc08159e3d660f414753c5158ad9a2325af
Ricardo Rocha <me@rrocha.uk>
2021-06-26 16:40:07 +0100

    :bug: bug fixes;

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ let logger = require('morgan');
 
 let cors = require('cors')
 
-let indexRouter = require('./routes/index');
-let usersRouter = require('./routes/users');
+// let indexRouter = require('./routes/index');
+// let usersRouter = require('./routes/users');
 let emailRouter = require('./routes/email')
 
 let app = express();
@@ -18,8 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 app.use('/api', emailRouter)
 
 module.exports = app;
--- routes/email.js
+++ routes/email.js
@@ -3,12 +3,22 @@ const router = express.Router();
 
 const {sendEmail} = require('../controllers/email')
 
-router.post('/mail', (req, res) => {
-
-    sendEmail(req.body)
-
-    res.send({message: 'email sent'})
+router.post('/mail', send, (req, res) => {
+    //TODO add bearer token to authenticate and not allow invalid email senders...
     res.end()
 })
 
 module.exports = router
+
+function send(req, res, next) {
+
+    if(!req.body.name || !req.body.email || !req.body.userMessage) {
+        res.send({message: 'invalid body...'})
+        next()
+        return
+    }
+
+    sendEmail(req.body)
+    res.send({message: 'email sent.'})
+    next()
+}
