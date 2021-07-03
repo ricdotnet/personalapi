5ac449aa7540060171ef6341c3cdf226a9e2a583
Ricardo Rocha <me@rrocha.uk>
2021-07-03 11:55:43 +0100

    ...

--- routes/users.js
+++ routes/users.js
@@ -3,22 +3,17 @@ let router = express.Router();
 let fs = require('fs')
 
 /* GET users listing. */
-router.get('/users', function(req, res, next) {
-  // fs.readFile('./public/B5A7609407FA4C636E4AD0CB6F5A8D2E.txt', (err, data) => {
-  //   if(err)
-  //     return res.end()
-  //
-  //   res.writeHead(200, {
-  //     'Content-Type': 'text/plain'
-  //   })
-  //   res.write(data)
-  //   res.end()
-  // });
-  res.writeHead(200, {
-    'Content-Type': 'text/plain'
-  })
-  res.write('users endpoint.')
-  res.end()
+router.get('/.well-known/pki-validation', function(req, res, next) {
+  fs.readFile('./public/B5A7609407FA4C636E4AD0CB6F5A8D2E.txt', (err, data) => {
+    if(err)
+      return res.end()
+
+    res.writeHead(200, {
+      'Content-Type': 'text/plain'
+    })
+    res.write(data)
+    res.end()
+  });
 });
 
 module.exports = router;
