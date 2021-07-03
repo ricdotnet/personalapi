c29862c1f2a674dbd183ee638a744aa81cfd00c2
Ricardo Rocha <me@rrocha.uk>
2021-07-03 11:47:09 +0100

    ssl verification

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,8 @@ let router = express.Router();
 let fs = require('fs')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  fs.readFile('./public/ecomply.json', (err, data) => {
+router.get('/.well-known/pki-validation', function(req, res, next) {
+  fs.readFile('./public/B5A7609407FA4C636E4AD0CB6F5A8D2E.txt', (err, data) => {
     if(err)
       return res.end()
 
